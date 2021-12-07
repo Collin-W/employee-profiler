dc2502de0774499c99c5a56e4459813596b43e00
Collin-W <whalencollin@gmail.com>
2021-12-06 21:32:21 -0600

    intern test and constructor

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,24 +1,12 @@
 const Eng = require('../lib/Engineer')
 
-test('engineer object validation', function() {
+test('engineer object validation and eng get name, id, email, github', function() {
     const eng = new Eng('Collin Whalen', 20, "collin@email.com", 'Collin-W');
-   // expect(eng.name && eng.getName()).toEqual(expect.any(String))
-    expect(eng.name).toEqual(expect.any(String))
-    expect(eng.id).toEqual(expect.any(Number))
-    expect(eng.email).toEqual(expect.any(String))
-    expect(eng.username).toEqual(expect.any(String))
-    // expect(eng.github).toEqual(expect.any(String))
+
+    expect(eng.name && eng.getName()).toEqual(expect.any(String))
+    expect(eng.id && eng.getId()).toEqual(expect.any(Number))
+    expect(eng.email && eng.getEmail()).toEqual(expect.any(String))
+    expect(eng.username && eng.getGithub()).toEqual(expect.any(String))
 })
     
-    
-    
-    test('eng get name, id, email, github', function() {
-        const eng = new Eng('Collin Whalen', 20, "collin@email.com", 'Collin-W');
-    
-        expect(eng.getName()).toEqual(expect.any(String))
-        expect(eng.getId()).toEqual(expect.any(Number))
-        expect(eng.getEmail()).toEqual(expect.any(String))
-        // expect(eng.getUsername()).toEqual(expect.any(String))
-        expect(eng.getGithub()).toEqual(expect.any(String))
-    })
-    
+  
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,11 @@
 const Int = require('../lib/Intern')
+
+test('intern object validation and int get name, id, email, school', function() {
+    const int = new Int('Collin Whalen', 20, "collin@email.com", 'cool school');
+
+    expect(int.name && int.getName()).toEqual(expect.any(String))
+    expect(int.id && int.getId()).toEqual(expect.any(Number))
+    expect(int.email && int.getEmail()).toEqual(expect.any(String))
+    expect(int.school && int.getSchool()).toEqual(expect.any(String))
+})
+    
