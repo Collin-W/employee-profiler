9e7d20080d486136fa5961c7c8dcd72b19598b02
Collin-W <whalencollin@gmail.com>
2021-12-06 22:23:51 -0600

    all tests pass finished manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,11 @@
-const Man = require('../lib/Manager')
+const Mana = require('../lib/Manager')
+
+test('manager object validation and manager get name, id, email, office number', function() {
+    const mana = new Mana('Collin Whalen', 20, "collin@email.com", 5);
+
+    expect(mana.name && mana.getName()).toEqual(expect.any(String))
+    expect(mana.id && mana.getId()).toEqual(expect.any(Number))
+    expect(mana.email && mana.getEmail()).toEqual(expect.any(String))
+    expect(mana.officeNum && mana.getOfficeNum()).toEqual(expect.any(Number))
+})
+    
